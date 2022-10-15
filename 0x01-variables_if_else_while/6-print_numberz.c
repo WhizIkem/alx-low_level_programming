e2755cb7b93ffe3b84df207aa9f231e05f8e41b8
WhizIkem <wisdomikem@gmail.com>
2022-10-15 14:31:55 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print all digit numbers starting from 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+	putchar((num % 10 + '0'));
+
+	putchar('\n');
+
+	return (0);
+}
