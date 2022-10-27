2054d553efd9798b2b5b42ad592c86020cd8cdc1
WhizIkem <wisdomikem@gmail.com>
2022-10-27 11:44:54 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * cap_string - a function that capitalizes all words of a string
  * @str: string
- * Return: pointer to string\
+ * Return: pointer to string
  */
 char *cap_string(char *str)
 {
@@ -14,11 +14,11 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
-str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
-str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
-str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
+		if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' ||
+str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
+str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' ||
+str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
 			str[i] -= 32;
 		i++;
 	}
