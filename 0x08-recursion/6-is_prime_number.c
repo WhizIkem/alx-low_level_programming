df725cb2c874db68f146c45c4a7aa0b269fb9ea9
WhizIkem <wisdomikem@gmail.com>
2022-11-04 17:37:30 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,10 +10,10 @@ int checker(int n, int base)
 {
 	if (base % n == 0 || base < 2)
 		return (0);
-	else if (n == base -1)
+	else if (n == base - 1)
 		return (1);
 	else if (base > n)
-		return (checker(n +1, base));
+		return (checker(n + 1, base));
 	return (1);
 }
 
