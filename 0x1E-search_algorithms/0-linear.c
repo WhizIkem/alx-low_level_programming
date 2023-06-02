f2300d0a22f23ee8626cbe6ce04a363315d3c190
whizikem <wisdomikem@gmail.com>
2023-06-02 19:25:41 +0000

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+
 	if (array == NULL) 
 	{
 		return (-1);
 	}
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", array[i], value);
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return i;  /*Found the value at index i*/
